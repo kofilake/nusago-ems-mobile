c86f857dfdca9d6b6a389c5b3a3d7dae3574ee9d
kofilake <agnarjethro69@gmail.com>
2026-06-16 13:54:38 +0700

    feat: registered local data source

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,31 +1,51 @@
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:nusago_ems/features/auth/data/data_sources/auth_local_data_source.dart';
+import 'package:nusago_ems/features/employees/data/data_sources/employee_remote_data_source.dart';
+import 'package:nusago_ems/features/employees/data/repositories/employee_repository_impl.dart';
+import 'package:nusago_ems/features/employees/domain/repositories/employee_repository.dart';
+import 'package:nusago_ems/features/employees/presentation/bloc/employee_list_bloc.dart';
 import 'features/auth/presentation/bloc/auth_bloc.dart';
 import 'features/auth/data/repositories/auth_repository_impl.dart';
 import 'features/auth/domain/repositories/auth_repository.dart';
 import 'features/auth/data/data_sources/auth_remote_data_source.dart';
-import 'core/network/dio_client.dart'; 
+
+import 'core/network/dio_client.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
-  // 1. Register BLoC
+  // FEATURES - AUTH
   sl.registerFactory(() => AuthBloc(authRepository: sl()));
 
-  // 2. Register Repository
   sl.registerLazySingleton<AuthRepository>(
-    () => AuthRepositoryImpl(remoteDataSource: sl()),
+    () => AuthRepositoryImpl(
+      remoteDataSource: sl(), 
+      localDataSource: sl()
+    ),
   );
 
-  // 3. Register Data Source (FIXED)
   sl.registerLazySingleton<AuthRemoteDataSource>(
-    // Changed this from AuthRepositoryImpl to AuthRemoteDataSource and passed in the sl() to fetch the DioClient.
-    () => AuthRemoteDataSource(dioClient: sl()), 
+    () => AuthRemoteDataSource(dioClient: sl()),
   );
 
-  // 4. Register Core/Network
+  sl.registerLazySingleton<AuthLocalDataSource>(
+    () => AuthLocalDataSource(secureStorage: sl()),
+  );
+
   sl.registerLazySingleton(() => DioClient());
 
-  // 5. Register External Packages (Dio)
   sl.registerLazySingleton(() => Dio());
+
+  sl.registerLazySingleton(() => const FlutterSecureStorage());
+
+  // FEATURES - EMPLOYEES
+  sl.registerFactory(() => EmployeeListBloc(repository: sl()));
+
+  sl.registerLazySingleton<EmployeeRepository>(
+    () => EmployeeRepositoryImpl(remoteDataSource: sl()),
+  );
+
+  sl.registerLazySingleton(() => EmployeeRemoteDataSource(dioClient: sl()));
 }
